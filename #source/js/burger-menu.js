3663e898ae070637faeb5144643b194c2de106a6
kalachlegends <90833872+kalachlegends@users.noreply.github.com>
2022-03-04 22:41:52 +0600

    Add files via upload

--- #source/js/burger-menu.js
+++ #source/js/burger-menu.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
- $('.header_burger').click(function(event){ //при клике на бургер что дожно происхоидть
- 	$('.header_burger,.navgation__header').toggleClass('active');//когда человек будет нажажимать на бургер будет добавляться класс active
+ $('.nav__burger,.close').click(function(event){ //при клике на бургер что дожно происхоидть
+ 	$('.nav__burger,.header,.close').toggleClass('active');//когда человек будет нажажимать на бургер будет добавляться класс active
  	$('body').toggleClass('lock');
  });
 });
--- #source/js/main.js
+++ #source/js/main.js
@@ -1,3 +1,6 @@
+//@prepros-append jquery.parallax.js
+//@prepros-append parallax.js
 //@prepros-append sticky-nav.js
 //@prepros-append burger-menu.js
 //@prepros-append swiper.js
+//@prepros-append script-parallax.js
