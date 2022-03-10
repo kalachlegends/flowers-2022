4869ffb621b9d1e61aa814767ca2174ec118ffc6
kalachlegends <90833872+kalachlegends@users.noreply.github.com>
2022-03-10 22:52:17 +0600

    Add files via upload

--- #source/js/burger-menu.js
+++ #source/js/burger-menu.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
- $('.nav__burger,.close').click(function(event){ //при клике на бургер что дожно происхоидть
+ $('.nav__burger,.close,.mobile__close').click(function(event){ //при клике на бургер что дожно происхоидть
  	$('.nav__burger,.mobile,.close').toggleClass('active');//когда человек будет нажажимать на бургер будет добавляться класс active
  	$('body').toggleClass('lock');
  });
